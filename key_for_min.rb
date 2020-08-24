7fecf3ffd6e3fd93b76a52e350e087788c308063
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-24 05:12:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,3 +5,17 @@ def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     
 end
+
+
+
+def key_for_min_value(name_hash)
+  array_ordered_by_age = []
+  array_ordered_by_age = name_hash.collect {|name, age|
+    age}.sort
+  name_hash.each do |name, age|
+    if age == array_ordered_by_age[0]
+      return name
+    end
+  end
+  nil 
+end
