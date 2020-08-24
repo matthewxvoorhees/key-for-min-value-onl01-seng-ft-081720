895822f80d6ae2bd30bd5ef115ff511fd511845f
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-24 05:13:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,3 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
   array_ordered_by_age = []
   array_ordered_by_age = name_hash.collect {|name, age|
